56f51d4866bce4b3563b33401229a009c0abc8b9
Sergio <34040046+Sergiowk@users.noreply.github.com>
2021-07-12 14:02:23 +0200

    Page component (#5)
    * Page component created
    
    * Sections created
    
    * ngx-page-scroll added and configured

--- sgj-portfolio/src/app/page/page.component.ts
+++ sgj-portfolio/src/app/page/page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { PageScrollService } from 'ngx-page-scroll-core';
 
 @Component({
   selector: 'app-page',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pageScrollService: PageScrollService, @Inject(DOCUMENT) 
+  private document: any) { }
 
   ngOnInit(): void {
+    this.pageScrollService.scroll({
+      document: this.document,
+      scrollTarget: '.aboutme',
+    });
   }
 
 }
