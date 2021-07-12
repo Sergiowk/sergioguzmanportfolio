4ca60e58f3cf6b004cfa996c75be86e10ed1c012
Sergiowk <sergiowk94@gmail.com>
2021-07-12 13:48:12 +0200

    ngx-page-scroll added and configured

--- sgj-portfolio/src/app/app.module.ts
+++ sgj-portfolio/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -14,7 +14,8 @@ import { PageComponent } from './page/page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPageScrollCoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
